27c05f5861e6574dac631c23965430d884ef33b0
Bernat Borràs Civil <70479573+BernatBC@users.noreply.github.com>
2024-11-10 15:56:05 +0100

    Make buttons interactable

--- Assets/Scripts/InteractiveButton.cs
+++ Assets/Scripts/InteractiveButton.cs
@@ -2,35 +2,60 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.InputSystem;
 
 public class InteractiveButton : MonoBehaviour
 {
 
-    bool selected = false;
+    public Material InactiveMaterial;
+    public Material GazedAtMaterial;
+    public bool fovToggler = false;
+    public bool changeTimeToggler = false;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private MeshRenderer _myRenderer;
 
-    // Update is called once per frame
-    void Update()
+    InputAction makeAction;
+
+    private bool selected = false;
+
+    public void Start()
     {
-        
+        makeAction = InputSystem.actions.FindAction("makeAction");
+        _myRenderer = GetComponent<MeshRenderer>();
+        SetMaterial();
     }
 
     public void OnPointerEnter()
     {
         selected = true;
-        gameObject.GetComponent<Image>().color = Color.blue;
-        Debug.Log("Object Selected");
+        SetMaterial();
     }
 
     public void OnPointerExit()
     {
         selected = false;
-        gameObject.GetComponent<Image>().color = Color.red;
-        Debug.Log("Object Unselected");
+        SetMaterial();
+    }
+
+    public void OnPointerClick()
+    {
+        Debug.Log("Grab object");
+    }
+
+    private void SetMaterial()
+    {
+        if (InactiveMaterial != null && GazedAtMaterial != null)
+        {
+            _myRenderer.material = selected ? GazedAtMaterial : InactiveMaterial;
+        }
+    }
+
+    private void Update()
+    {
+        if (selected && makeAction.ReadValue<float>() > 0.1)
+        {
+            if (fovToggler) Debug.Log("Toggling FOV");
+            if (changeTimeToggler) Debug.Log("Changing light");
+        }
     }
 }
